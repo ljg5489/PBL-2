de83fa399c3abced376710c9f0c28297f38012fd
ljg5489 <ljg5489@naver.com>
2025-11-11 12:59:59 +0900

    LibraryApplication, Loan 클래스 추가

---
+++ LibraryApplication.java
@@ -0,0 +1,25 @@
+
+/**
+ * LibraryApplication 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class LibraryApplication
+{
+    private String name;
+    Book[] 수장자료;
+    Borrower[] 이용자;
+    Loan[] 대출기록;
+    public 대출한다(){
+        
+    }
+    
+    public 화면에출력한다(){
+        
+    }
+    
+    public 이용자를등록한다(name){
+        
+    }
+}
---
+++ Loan.java
@@ -0,0 +1,14 @@
+
+/**
+ * Loan 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Loan
+{
+    Borrower[] 대출자;
+    Book[] 대출책;
+    private date 대출일시;
+    private date 반납일시;
+}
