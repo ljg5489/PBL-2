4ba9bb406d07b12bcf6975933cdfaa075c6340b5
전채금 <chaegeujeon@gamil.com>
2025-12-08 21:56:44 +0900

    이름 추가

--- FrontGUI/RegBookFrame.java
+++ FrontGUI/RegBookFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 /**
  * RegBookFrame 클래스: 책 등록을 위한 프레임
  *
- * @author (2022320005 이진규, 2022320009 이상원, 2024320060 전채금)
+ * @author (2022320005 이진규, 2022320009 이상원)
  * @version (2025.12.2)
  */
 public class RegBookFrame extends JFrame
